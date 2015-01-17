777f861b358a364aa9a5630b1bea4fdb0b772ec2
Colin Craft <colin@colincraft.com>
2015-01-17 15:54:13 -0800

    second iteration. need to add win function

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+//add win and draw functionality.
+
+
 function start() {
 	makeBoxesClickable();
 	document.getElementById("reset").addEventListener("click", resetEverything);
-	
+	win()
 }
 var boxes = document.getElementsByClassName("box")
 
@@ -14,23 +17,38 @@ function makeBoxesClickable(){
 var turn = 0;
 
 function turnCounter(event){
-	if (turn % 2 == 0 ){
-			event.target.setAttribute("class", "X");
-		 }if ( turn % 2 == 1){
-			event.target.setAttribute("class", "O");
+	if (turn % 2 == 0 && document.getElementsByClassName("box")){
+			event.target.className += " X";
+			document.getElementById("whosTurn").innerHTML = "X's turn";
+			this.removeEventListener("click", turnCounter);
+			
+		 }if ( turn % 2 == 1 && document.getElementsByClassName("box")){
+			event.target.className += " O";
+			document.getElementById("whosTurn").innerHTML = "O's turn";
+			this.removeEventListener("click", turnCounter);
+			
 		}
-		turn++;
+		turn++
 }
 
 // document.getElementsByTagName("button").addEventListener("click", resetEverything);
 
 function resetEverything(){
 	var boxeReset = document.querySelectorAll("div.X, div.O")
+	document.getElementById("whosTurn").innerHTML = " ";
 
 	for (var i = 0; i < boxeReset.length; i++) {
 		boxeReset[i].className = "box";
+		makeBoxesClickable();
 		event.preventDefault();
 	}
 }
+
+
+
+
+
+
+
 
 window.onload = start;
